46bff9bc59da0af5055bbd871fa35dcb85e03b02
kisborg <mark.borgstrom@gmail.com>
2021-01-01 15:09:12 +0100

    implemented firebase utils, added storing autheticated users in firebase database

--- src/App.js
+++ src/App.js
@@ -6,17 +6,28 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth } from './firebase/firebase.utils';
+import { createUserProfileDocument } from './firebase/firebase.utils';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
 
-  useEffect(() => {
-    let unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      setCurrentUser(user);
-      console.log(user);
+  const onAuthStateChange = () => {
+    let unsubscribe = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          })
+        });
+      } 
+      setCurrentUser(userAuth);
     });
-    return () => unsubscribeFromAuth();
-  }, []);
+    return () => unsubscribe();
+  }
+  useEffect(onAuthStateChange, []);
+  useEffect(() => console.log(currentUser), [currentUser]);
   return (
     <div>
       <Header currentUser={currentUser} />
---
+++ src/components/sign-up/sign-up.component.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import './sign-up.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+export default function SignUp() {
+  const [state, setState] = useState({
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+
+      await createUserProfileDocument(user, { displayName }); 
+      
+      setState({
+        displayName: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      });
+      
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState(prevState => ({ ...prevState, [name]: value}));
+  };
+  
+  const { displayName, email, password, confirmPassword } = state;
+  return (
+    <div className="sign-up">
+      <h2 className="title">I do not have an account</h2>
+      <span>Sign up with your email and password</span>
+      <form className="sign-up-form" onSubmit={handleSubmit}>
+        <FormInput
+          type="text"
+          name="displayName"
+          value={displayName}
+          onChange={handleChange}
+          label='Display Name'
+          required
+        />
+        <FormInput
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          label='Email'
+          required
+        />
+        <FormInput
+          type="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+          label='Password'
+          required
+        />
+        <FormInput
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={handleChange}
+          label='Confirm Password'
+          required
+        />
+        <CustomButton type="submit">SIGN UP</CustomButton>
+      </form>
+      
+    </div>
+  )
+}
