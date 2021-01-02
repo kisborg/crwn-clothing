8bd049a8008ed5c58f2d05e16868b2f41d2dbe5f
kisborg <mark.borgstrom@gmail.com>
2021-01-02 21:27:19 +0100

    added addItem and removeItem functionality in cart reducer and implemented arrows in checkout items

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -10,3 +10,16 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToRemove.id);
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((item) => item.id !== cartItemToRemove.id);
+  }
+  return cartItems.map((item) => {
+    if (item.id === cartItemToRemove.id) {
+      return { ...item, quantity: item.quantity - 1 }
+    }
+    return item;
+  })
+}
