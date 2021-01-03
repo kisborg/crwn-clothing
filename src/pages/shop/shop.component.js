b10fd06a97f3563b30a46e28deb342dce60f2ea3
kisborg <mark.borgstrom@gmail.com>
2021-01-03 16:17:11 +0100

    moved directory and shop data to reducers, created selectors for collections/directory data, and created respective data flow in shop/directory

--- src/pages/shop/shop.component.js
+++ src/pages/shop/shop.component.js
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-export default function ShopPage() {
+import React from 'react';
+import { Route } from 'react-router-dom';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-  const [collections] = useState(SHOP_DATA);
+export default function ShopPage({ match }) {
   return (
     <div>
-      {
-        collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))
-      }
+      <Route exact path ={`${match.path}`} component={ CollectionsOverview } />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   )
 }
+
